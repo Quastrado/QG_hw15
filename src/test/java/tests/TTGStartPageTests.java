f5ac5363b2c0074a77af5ca85c6584c7c0fbd778
Quastrado <Ataxyswriter@yandex.ru>
2026-06-16 20:28:00 +0300

    add @DisplayName

--- src/test/java/tests/TTGStartPageTests.java
+++ src/test/java/tests/TTGStartPageTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import tests.testdata.TestDataTTGClub;
 
@@ -12,6 +13,7 @@ public class TTGStartPageTests extends TestBase {
 
 
     @Test
+    @DisplayName("Checking a simple search script from the main page")
     void  displaySearchResult() {
         SelenideElement searchResultPageHeader = ttgClubStartPage.searchFromPage(
           testData.searchQuery,
@@ -21,30 +23,35 @@ public class TTGStartPageTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Checking the navigation menu display")
     void checkOpenNavigationModal() {
         SelenideElement navigationModal = ttgClubStartPage.openNavigationMenu();
         navigationModal.shouldBe(visible);
     }
 
     @Test
+    @DisplayName("Checking the display of the bookmarks list")
     void checkOpenBookmarksList() {
         SelenideElement bookmarksListTitle = ttgClubStartPage.openBookmarksList();
         bookmarksListTitle.shouldHave(text(testData.bookmarksExpectedTitle));
     }
 
     @Test
+    @DisplayName("Checking the script for navigating to a class page from the main page")
     void checkGoingToTheClassPage() {
         SelenideElement classArchetypesList = ttgClubStartPage.goingToTheClassPage(testData.choosenClass);
         classArchetypesList.shouldHave(visible);
     }
 
     @Test
+    @DisplayName("Checking the transition to the Tokenator page")
     void checkGoingToTheTokenator() {
         SelenideElement tokenatorPageTitle = ttgClubStartPage.goingToTokenator();
         tokenatorPageTitle.shouldHave(visible);
     }
 
     @Test
+    @DisplayName("Checking the working out of the formula calculator")
     void checkFormulaCalculate() {
         SelenideElement calculateResult = ttgClubStartPage.formulaCalculate(
                 testData.formula
@@ -53,6 +60,7 @@ public class TTGStartPageTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Checking the opening of a modal window with a video on the main page")
     void openVideoPlayerModal() {
         String videoTitle = ttgClubStartPage.getVideoLink().text();
         SelenideElement videoModalTitle = ttgClubStartPage.openVideo();
@@ -60,6 +68,7 @@ public class TTGStartPageTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Checking to switch to a dark color theme")
     void checkDarkThemeTurning() {
         SelenideElement background = ttgClubStartPage.turnDarkTheme();
         background.shouldHave(cssValue(
@@ -69,12 +78,14 @@ public class TTGStartPageTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Checking the transition to the search page")
     void checkGoingToTheSearchPage() {
         SelenideElement searchedPageTitle = ttgClubStartPage.goingToTheSearchPage();
         searchedPageTitle.shouldHave(text(testData.searchPageTitle));
     }
 
     @Test
+    @DisplayName("Checking the display of the bug message window")
     void reportAboutBug() {
         SelenideElement bugReportModalTitle = ttgClubStartPage.openBugReportModal();
         bugReportModalTitle.shouldHave(text(testData.bugReportModalTitle));
